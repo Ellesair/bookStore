bf38a5f31f01e8f91b849e6a016e4c0d89c53f65
K Lake <klake.coding@gmail.com>
2022-01-11 20:45:54 -0600

    fixed EVERYTHING

---
+++ index.js
@@ -0,0 +1,14 @@
+
+function openNav() {
+    let x = document.getElementById("vertical-menu")
+    
+    if (x.style.width === "170px"){
+        x.style.width = "0px"
+    }
+    else {
+        x.style.width = "170px"
+    }
+
+  }
+  
+  
