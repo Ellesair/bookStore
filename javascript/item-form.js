27c12899e2a522b8fb7db263fd4982fe8e0f5323
mdbrown543 <mdbrown543@gmail.com>
2022-02-23 13:06:15 -0600

    Added names to inputs in item_form.html

--- javascript/item-form.js
+++ javascript/item-form.js
@@ -1,13 +1,12 @@
 // Initialize a new ItemsController with currentId set to 0
-const itemsController = new ItemsController(0);
+const itemsController = new itemsController(0);
 
 // Select the New Item Form
 const newItemForm = document.querySelector('#newItemForm');
 
 // Add an 'onsubmit' event listener
 newItemForm.addEventListener('submit', (event) => {
-    // Prevent default action
-    event.preventDefault();
+    
 
     // Select the inputs
     const newItemName = document.querySelector('#newItemName');
@@ -19,9 +18,21 @@ newItemForm.addEventListener('submit', (event) => {
     const description = newItemDescription.value;
     const imageUrl = newItemImageUrl.value;
 
-    /*
-        Validation code here
-    */
+    
+    // Validation code here
+    function validateForm() {
+        let x = document.forms["myForm"]["name"].value;
+        if (x == "") {
+            alert("Field must be filled out");
+
+                // Prevent default action
+                event.preventDefault();
+
+            return false;
+         }
+     }
+    
+    
 
     // Add the item to the ItemsController
     itemsController.addItem(name, description, imageUrl);
